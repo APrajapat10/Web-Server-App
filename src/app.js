44389d33b604caa16ba3b5cb9abad753168072e6
APrajapat10 <coolprajapat@gmail.com>
2022-02-09 11:58:48 +0530

    Handlebars work done

--- src/app.js
+++ src/app.js
@@ -1,21 +1,38 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "./public");
+
+app.use(express.static(publicDirectoryPath));
+
+app.set("view engine", "hbs");
 
 app.get("", (req, res) => {
-  res.send("Hello Express");
-});
-
-app.get("/help", (req, res) => {
-  res.send("Help Needed");
+  res.render("index", {
+    title: "Weather",
+    name: "Abhishek Prajapat",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Us");
+  res.render("about", {
+    title: "About Me",
+    name: "Abhishek Prajapat",
+  });
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather is good");
+  res.send({
+    location: "Kolkata",
+    forecast: "Snowing",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "From a help file",
+  });
 });
 
 app.listen(3000, () => {
