1c872517cd02007baf0139cb285902457b7794b7
APrajapat10 <coolprajapat@gmail.com>
2022-02-11 14:10:26 +0530

    Bug Fixed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, "./public");
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
