ccbde33c671b4904cb0ecd139065588acb765edb
APrajapat10 <coolprajapat@gmail.com>
2022-02-16 14:54:21 +0530

    Input Successfully Taken

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
 console.log("Client side JS loaded");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  console.log(location);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log("Location: " + data.location);
+          console.log("Forecast: " + data.forecast);
+        }
+      });
+    }
+  );
+});
