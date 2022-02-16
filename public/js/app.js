b453672c6774e4c6fc12d384740484b3798823a1
APrajapat10 <coolprajapat@gmail.com>
2022-02-16 15:15:53 +0530

    Final Styling done

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,23 @@ console.log("Client side JS loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
-  console.log(location);
+  messageOne.textContent = "Loading!";
+  messageTwo.textContent = "";
   fetch(`http://localhost:3000/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          messageOne.textContent = data.error;
         } else {
-          console.log("Location: " + data.location);
-          console.log("Forecast: " + data.forecast);
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
       });
     }
